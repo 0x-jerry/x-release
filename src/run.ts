f6fd9f4719bd1d40dd312ee59848a050fe821ffd
Jerry Wang <x.jerry.wang@gmail.com>
2021-12-29 22:14:57 +0800

    fix: run command

--- src/run.ts
+++ src/run.ts
@@ -4,5 +4,5 @@ import pc from 'picocolors'
 export async function run(cmd: string) {
   console.log(pc.dim('$'), pc.dim(cmd))
 
-  await execa('sh', ['-c', cmd], { stdio: 'pipe' })
+  await execa('sh', ['-c', cmd], { stdio: 'inherit' })
 }
