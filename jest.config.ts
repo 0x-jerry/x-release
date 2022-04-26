c53a7a9edaaaaf4d618e748a47280d504a66a67b
0x-jerry <x.jerry.wang@gmail.com>
2022-04-26 15:22:34 +0800

    ci: use vitest instead of jest

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types'
-
-const conf: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-}
-
-export default conf
