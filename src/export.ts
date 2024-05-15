5aa2874052f76e07e1bf80e209f427c84791c882
Jerry Wang <x.jerry.wang@gmail.com>
2024-05-15 20:50:32 +0800

    docs: add config page in docs

--- src/export.ts
+++ src/export.ts
@@ -2,6 +2,13 @@ import type { UserConfig } from './types'
 
 export type { UserConfig }
 
+/**
+ *
+ * Config file path `release.conf.ts`
+ *
+ * @param conf
+ * @returns
+ */
 export function defineConfig(conf: UserConfig) {
   return conf
 }
