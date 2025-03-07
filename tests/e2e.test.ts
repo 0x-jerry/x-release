33c45d14ccc1efcd8053557c9df0daf77a59eade
Jerry <x.jerry.wang@gmail.com>
2025-03-07 02:58:58 +0000

    test: resolve config

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,18 +1,33 @@
-import { exec } from '@0x-jerry/utils/node'
+import { exec, readJson } from '@0x-jerry/utils/node'
 import { runInProjectDir } from './utils'
-import { readFile } from 'node:fs/promises'
-
-const TestProjectName = 'release-conf'
+import { resolveConfig } from '../src/config'
 
 describe('e2e', () => {
-  it('relese config', async () => {
-    await runInProjectDir(TestProjectName, async () => {
+  it('resolve config', async () => {
+    await runInProjectDir('resolve-config', async () => {
+      const resp = await resolveConfig({})
+      expect(resp.beforeCommit).instanceOf(Function)
+      expect(resp['~test']?.runner).instanceOf(Function)
+    })
+  })
+
+  it('release workflow', async () => {
+    await runInProjectDir('release-workflow', async () => {
       const cliScriptPath = '../../../src/cli.ts'
       await exec(`npx tsx ${cliScriptPath} --patch`)
 
-      const data = await readFile('data.json')
+      const data = await readJson('data.json')
 
-      expect(data).toEqual(['before commit', 'todo', 'done'])
+      expect(data).toEqual([
+        'before commit',
+        'git add .',
+        'git commit -m "chore: release test@v1.0.1"',
+        'git tag "test@v1.0.1"',
+        'git push',
+        'git push --tags',
+        'npm publish',
+        'done',
+      ])
     })
   })
 })
